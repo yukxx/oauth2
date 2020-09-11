56ed92b5a17104f7b33a463094b23aee1df1220d
yukx <ykx694883436@gmail.com>
2020-09-11 15:14:21 +0800

    配置config

---
+++ yukx_service/src/main/java/yukx/security/service/ServiceApplication.java
@@ -0,0 +1,13 @@
+package yukx.security.service;
+
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * @ClassName ServiceApplication
+ * @Description TODO
+ * @Author yukx
+ * @Date 2020-09-11 15:01
+ **/
+@SpringCloudApplication
+public class ServiceApplication {
+}
