d9dc37e6a8252e34cfb5b03aa96cd2abdcf00208
yukx <ykx694883436@gmail.com>
2020-09-15 09:11:40 +0800

    oauth2

---
+++ yukx_auth/src/main/java/yukx/security/auth/AuthApplication.java
@@ -0,0 +1,20 @@
+package yukx.security.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName AuthApplication
+ * @Description TODO
+ * @Author yukx
+ * @Date 2020-09-14 17:32
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
---
+++ yukx_auth/src/main/java/yukx/security/auth/config/AuthorizationServerConfig.java
@@ -0,0 +1,52 @@
+package yukx.security.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+/**
+ * @ClassName AuthorizationServerConfig
+ * @Description TODO
+ * @Author yukx
+ * @Date 2020-09-14 17:56
+ **/
+@Configuration
+@EnableAuthorizationServer // 配置授权服务
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    /**
+     * 配置AuthorizationServer安全认证的相关信息，创建ClientCredentialsTokenEndpointFilter核心过滤器
+     *
+     * @param security
+     * @throws Exception
+     */
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        super.configure(security);
+    }
+
+    /**
+     * 配置OAuth2的客户端相关信息
+     *
+     * @param clients
+     * @throws Exception
+     */
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient("");
+    }
+
+    /**
+     * 配置AuthorizationServerEndpointsConfigurer众多相关类，包括配置身份认证器，配置认证方式，TokenStore，TokenGranter，OAuth2RequestFactory
+     *
+     * @param endpoints
+     * @throws Exception
+     */
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        super.configure(endpoints);
+    }
+}
