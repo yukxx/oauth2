c5e20dc99aa9ec855fb3b8c6cef4ebcf52fd7821
yukx <ykx694883436@gmail.com>
2020-11-12 10:39:58 +0800

    logback

---
+++ yukx_service/src/main/java/yukx/security/service/dao/mappers/RouteMapper.java
@@ -0,0 +1,16 @@
+package yukx.security.service.dao.mappers;
+
+import yukx.security.service.dao.entity.Route;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author yukx
+ * @since 2020-10-29
+ */
+public interface RouteMapper extends BaseMapper<Route> {
+
+}
