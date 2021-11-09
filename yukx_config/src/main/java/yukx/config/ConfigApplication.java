240d99030beab18fa2495ecd28e616badaf33482
yukx <ykx694883436@gmail.com>
2021-11-09 17:49:46 +0800

    工具类

--- yukx_config/src/main/java/yukx/config/ConfigApplication.java
+++ yukx_config/src/main/java/yukx/config/ConfigApplication.java
@@ -17,6 +17,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello");
         SpringApplication.run(ConfigApplication.class, args);
     }
 }
